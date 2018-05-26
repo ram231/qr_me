d54435f6112bfe6ce135a1d2b17b2a92251d9922
ram231 <blusea231@gmail.com>
2018-05-26 11:22:31 +0800

    Fixed UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'home.dart';
 
 void main() => runApp(MyApp());
-
 
 class MyApp extends StatelessWidget {
   @override
@@ -20,7 +21,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
 class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => new _LoginPageState();
@@ -31,58 +31,64 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      
-      body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-              new Text("QR Me",style: TextStyle(
+        body: new Center(
+      child: new Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new Text(
+            "QR Me",
+            style: TextStyle(
               fontSize: 32.0,
               fontWeight: FontWeight.bold,
-              
             ),
           ),
           new Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 64.0,vertical: 12.0),
+            padding:
+                const EdgeInsets.symmetric(horizontal: 64.0, vertical: 12.0),
             child: TextField(
-              controller: _controller,
-              decoration: InputDecoration(
-                labelStyle: TextStyle( color: Colors.grey),
-                labelText: "Username",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(const Radius.circular(10.0)),
-                  
-                )
-                
-              )
-            ),
+                controller: _controller,
+                decoration: InputDecoration(
+                    labelStyle: TextStyle(color: Colors.grey),
+                    labelText: "Username",
+                    border: OutlineInputBorder(
+                      borderRadius:
+                          BorderRadius.all(const Radius.circular(10.0)),
+                    ))),
           ),
           new Padding(
             padding: const EdgeInsets.symmetric(horizontal: 64.0),
             child: TextField(
               controller: _controller,
               decoration: InputDecoration(
-                labelText: "Password",
-                labelStyle: TextStyle(color: Colors.white30),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(const Radius.circular(10.0))
-                )
-              ),
+                  labelText: "Password",
+                  labelStyle: TextStyle(color: Colors.white30),
+                  border: OutlineInputBorder(
+                      borderRadius:
+                          BorderRadius.all(const Radius.circular(10.0)))),
               obscureText: true,
             ),
           ),
           new Padding(
-            padding: const EdgeInsets.symmetric(vertical:32.0),
-            child: RaisedButton(
-              child: Text("Log in"),
-              onPressed: (){
-                Navigator.of(context).push(new MaterialPageRoute(builder: (context) => MyHomePage()));
-              },
-            ),
-          )
-          ],
-        ),
-      )
-    );
+              padding: const EdgeInsets.symmetric(vertical: 32.0),
+              child: new Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  FlatButton(
+                      child: Text("Exit"),
+                      onPressed: () {
+                        exit(0);
+                      }),
+                  RaisedButton(
+                    child: Text("Log in"),
+                    onPressed: () {
+                      Navigator.of(context).push(new MaterialPageRoute(
+                          builder: (context) => MyHomePage()));
+                    },
+                  ),
+                ],
+              ))
+        ],
+      ),
+    ));
   }
 }
